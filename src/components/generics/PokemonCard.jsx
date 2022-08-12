326be22a099d04db189c4fc41395308712c65325
Bryan <bryanferrando59@gmail.com>
2022-08-12 10:16:50 +0200

    Changes

--- src/components/generics/PokemonCard.jsx
+++ src/components/generics/PokemonCard.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import LoadingSpinner from '../layouts/LoadingSpinner';
 
-const PokemonCard = ({url}) => {
+const PokemonCard = ({url, type}) => {
   const [pokemon, setPokemon] = useState(null);
   const [pokemonSprites, setPokemonSprites] = useState("");
   useEffect(() => {
@@ -9,18 +10,25 @@ const PokemonCard = ({url}) => {
       axios.get(url).then(({data}) => setPokemon(data));
   }, [url]);
   useEffect(() => {
-    if(pokemon)
-      axios.get(pokemon?.forms[0]?.url).then(({data}) => setPokemonSprites(data.sprites));
+    if(pokemon && pokemon.forms[0]?.url)
+      axios.get(pokemon.forms[0].url).then(({data}) => setPokemonSprites(data.sprites));
   }, [pokemon]);
   
-  return (
-    <div className="pokemon-card position-relative d-flex justify-content-center align-items-center img-thumbnail">
+  return (<>
+    {(type === 'simple' &&
+    (<div className="pokemon-card position-relative d-flex justify-content-center align-items-center img-thumbnail">
+      {(!pokemon?.forms[0] && <LoadingSpinner />)}
       {(pokemonSprites && <img src={pokemonSprites.front_default} alt={pokemon.name} className="img-fluid" />)}
       <div className="infos position-absolute d-flex flex-column justify-content-center align-items-center w-100 h-100 text-light">
         {(pokemon && <span>{pokemon.name}</span>)}
       </div>
-    </div>
-  );
+    </div>)
+    )}
+    {(type === 'advanced' &&
+      <div className='pokemon-card w-100 h-100 border border-dark rounded p-2'>
+        {(pokemonSprites && <img src={pokemonSprites.front_default} alt={pokemon.name} className="img-fluid" />)}
+      </div>)}
+  </>);
 };
 
 export default PokemonCard;
--- src/components/layouts/Pokemons.jsx
+++ src/components/layouts/Pokemons.jsx
@@ -2,9 +2,11 @@ import React, { useRef, useState, useCallback } from 'react';
 import usePokemonSearch from '../../js/usePokemonSearch';
 import PokemonCard from '../generics/PokemonCard';
 import LoadingSpinner from './LoadingSpinner';
+import '../../styles/components/Pokemons.scss'
 import '../../styles/components/PokemonCard.scss';
 
 const Pokemons = (props) => {
+  const [type, setType] = useState('simple');
   const [offset, setOffset] = useState(0);
   const {loading, error, pokemons, next} = usePokemonSearch(offset);
   const observer = useRef();
@@ -16,7 +18,7 @@ const Pokemons = (props) => {
     
     observer.current = new IntersectionObserver(entries => {
       if(entries[0].isIntersecting && next){
-        setOffset(offset + 100);
+        setOffset(offsetValue => offsetValue + 100);
       }
     });
     
@@ -26,19 +28,24 @@ const Pokemons = (props) => {
   
   return (
     <div className='container my-5'>
+      <div className='nav display-type my-3 rounded'>
+        <img src="./applications.png" alt="simple" className={type ==='simple' ? 'active' : ''} onClick={() => setType('simple')}/>
+        <img src="./aligner-justifier.png" alt="advanced" className={type ==='advanced' ? 'active' : ''} onClick={() => setType('advanced')}/>
+      </div>
       <div className='d-flex flex-wrap gap-3 justify-content-center'>
         {
           pokemons.map((pokemonUrl, index) => {
             if(pokemons.length === index + 1){
               return <div key={index + 'Loader'} ref={loader}><PokemonCard key={index} url={pokemonUrl} /></div>
             } else {
-              return <PokemonCard key={index} url={pokemonUrl} />
+              return <PokemonCard key={index} url={pokemonUrl} type={type} />
             }
           })
         }
       </div>
       <div className="my-5 text-center">
         {(loading && (<LoadingSpinner />))}
+        {(error && <span>{error}</span>)}
       </div>
     </div>
   );
